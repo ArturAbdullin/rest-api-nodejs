dac908281141ca56bde3557b0c39dc1618d3b610
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-11 15:23:44 +0300

    feat(models): add a basic vehicle model
    the model is a vehicle database

---
+++ models/vehicleModel.js
@@ -0,0 +1,19 @@
+// import local database (.json file)
+/**
+ * @type {{id: number, vehicle: string, price: string, description: string}[]}
+ */
+const vehicles = JSON.parse(require("../data/vehicles"));
+
+/**
+ * Return a Promise to return all vehicles in the database
+ * @returns {Promise<vehicles>}
+ */
+function findAll() {
+  return new Promise((resolve, reject) => {
+    resolve(vehicles);
+  });
+}
+
+module.exports = {
+  findAll,
+};

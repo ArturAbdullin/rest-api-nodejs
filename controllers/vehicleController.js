cad7257782354f83e0b4207a473c6b36e146cbab
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-11 15:45:15 +0300

    feat(controllers): add a basic vehicle database controller

---
+++ controllers/vehicleController.js
@@ -0,0 +1,35 @@
+// these are just for the typing sake
+const IncomingMessage = require("http").IncomingMessage;
+const ServerResponse = require("http").ServerResponse;
+
+const VehicleDatabase = require("../models/vehicleModel");
+
+/**
+ * Return a Promise to return vehicles from the database
+ * @param {IncomingMessage} req
+ * @param {ServerResponse} res
+ */
+async function getVehicles(req, res) {
+  try {
+    // findAll returns a Promise -> use await
+    const vehicles = await VehicleDatabase.findAll();
+
+    // write to the response head
+    // status OK = 200
+    // Content-Type = applicaton/json as we return .json data
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    // write and simultaneously end the response
+    res.end(JSON.stringify(vehicles));
+    // the above line is a shorthand for
+    // res.write(JSON.stringify(vehicles));
+    // res.end();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  getVehicles,
+};

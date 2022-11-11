52bbd233ec58424291a0c96343bc91640c7e7391
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-11 16:06:01 +0300

    feat(server): add a basic http server config

---
+++ server.js
@@ -0,0 +1,13 @@
+// import a built-in http node module
+const http = require("http");
+
+// create an http server
+const server = http.createServer((req, res) => {});
+
+// create a port to run the server on
+// check if there is an environments port variable
+// if there isn't then use port 5000
+const PORT = process.env.PORT || 5000;
+
+// start listening on the configured port
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
